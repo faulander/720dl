51504b678b97ec272eb701621ea9b0acf68fcdaf
Harald Fauland <harald.fauland@gmail.com>
2022-09-30 16:36:29 +0200

    include and exclude added to settings

--- src/720pdl.py
+++ src/720pdl.py
@@ -1,17 +1,19 @@
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
 from pathlib import Path
 import requests
 import os,sys
-from qbclient import Client
 from loguru import logger
 import configparser
+import pendulum as p
+import feedparser
 
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+from qbclient import Client
 
 
 #Variable Definition
@@ -20,6 +22,7 @@ topicstovisit = list()
 completedsave = list()
 completed = list()
 fileCompleted = "completed.txt"
+today = p.now()
 
 logger.add("720pdl.log", format="{time} - {level} - {message}")
 
@@ -34,17 +37,21 @@ def cfgfile():
         config.add_section('Torrent')
         config.set('Default', 'savedir', str(Path.cwd()))
         config.set('Default', 'url', 'http://720pier.ru/search.php?search_id=active_topics&sr=topics&ot=1&pt=t&fid[]=46')
+        config.set('Default', 'useRSS', "yes")
+        config.set('Default', 'categories', '48')
+        config.set('Default', 'skipOlderThan', p.start_of("year").format("YYYY-MM-DD"))
+        config.set('Default', 'include', '')
+        config.set('Default', 'exclude', '')
         config.set('Credentials', 'username', '<your 720pier.ru username>')
         config.set('Credentials', 'password', '<your 720pier.ru password>')
         config.set('Torrent', 'sendToQBittorrent', "no")
         config.set('Torrent', 'qbclient', 'http://127.0.0.1:8080')  # WEB-Access for qBittorrent must be available
-        config.set('Torrent', 'category', '720pier')  # no category as standard
+        config.set('Torrent', 'category', '720pier')  # 
         config.set('Torrent', 'login', 'admin')  # Login to qBittorrent Client
         config.set('Torrent', 'password', 'admin')  # Password to qBittorrent Client
         config.set('Torrent', 'BasicAuthLogin', '')  # Basic Auth
         config.set('Torrent', 'BasicAuthPassword', '')  # Basic Auth
         config.set('Torrent', 'startPaused', 'yes')  # Start Downloads immediately (yes/no)
-
         config.write(cfgfile)
         cfgfile.close()
 
@@ -53,6 +60,12 @@ config = configparser.ConfigParser()
 cfgfile()
 config.read('720pier.ru.ini')
 cfgSaveDir=Path(config['Default']['savedir'])
+cfgURL=config['Default']['url']
+cfgUseRSS = config['Default'].getboolean('useRSS')
+cfgCategories = config['Default']['categories'].split(',')
+cfgSkipOlderThan = p.parse(config['Default']['skipOlderThan'])
+cfgInclude=config['Default']['include']
+cfgExclude=config['Default']['exclude']
 cfgUsername=config['Credentials']['username']
 cfgPassword=config['Credentials']['password']
 cfgSendToQb=config['Torrent'].getboolean('sendToQBittorrent')
@@ -62,7 +75,7 @@ cfgQbUsername=config['Torrent']['login']
 cfgQbPassword=config['Torrent']['password']
 cfgBasicAuthLogin=config['Torrent']['BasicAuthLogin']
 cfgBasicAuthPassword=config['Torrent']['BasicAuthPassword']
-cfgQbDownload=config['Torrent']['startPaused']
+cfgQbDownload=config['Torrent'].getboolean('startPaused')
 
 
 def readFinishedCrawls():
@@ -104,7 +117,6 @@ def getNewTorrents():
     options.add_argument("--headless")
     #options.add_argument("");
     service = Service(log_path=os.path.devnull)
-    #service = Service(log_path="")
     browser = WebDriver(service=service, options=options)
     logger.info("Firefox Preferences set.")
 
@@ -130,8 +142,7 @@ def getNewTorrents():
 
     try:
         #Open the Last Topics
-        browser.get('http://720pier.ru/search.php?search_id=active_topics&sr=topics&ot=1&pt=t&fid[]=46')
-        #browser.get('http://720pier.ru/viewforum.php?f=88')
+        browser.get(cfgURL)
     except Exception as e:
         if hasattr(e, 'message'):
             logger.error(e.message)
@@ -141,19 +152,72 @@ def getNewTorrents():
 
 
     #Find all Topics and their Links
-    topics = browser.find_elements(By.CLASS_NAME,'topictitle')
-    for topic in topics:
-        #logger.debug(topic)
-        for c in completed:
-            if c==str(topic.get_attribute('href')):
-                visited=True
-                completedsave.append(c)
-                logger.info(f"Topic has already been visited: {c}")
-        if not visited:
-            topicstovisit.append(topic.get_attribute('href'))
-            completedsave.append(topic.get_attribute('href'))
-            logger.info(f"Topic added to list: {topic.get_attribute('href')}")
-        visited=False
+    topics = list()
+    if not cfgUseRSS:
+        #read directly from web 
+        topics = browser.find_elements(By.CLASS_NAME,'topictitle')
+        for topic in topics:
+            #logger.debug(topic)
+            for c in completed:
+                if c==str(topic.get_attribute('href')):
+                    visited=True
+                    completedsave.append(c)
+                    logger.info(f"Topic has already been visited: {c}")
+            if not visited:
+                topicstovisit.append(topic.get_attribute('href'))
+                completedsave.append(topic.get_attribute('href'))
+                logger.info(f"Topic added to list: {topic.get_attribute('href')}")
+            visited=False
+    else:
+        #logger.debug(cfgCategories)
+        includedList = cfgInclude.split(",")
+        excludedList = cfgExclude.split(",")
+        baseFeedURL = "https://720pier.ru/feed/forum/"
+        for category in cfgCategories:
+            feed = feedparser.parse(baseFeedURL + str(category))
+            #logger.debug(feed)
+            for entry in feed.entries:
+                isIncluded = False
+                isExcluded = False
+                #logger.debug(p.parse(entry.published))
+                #logger.debug(entry.link)
+                if cfgSkipOlderThan <= p.parse(entry.published):
+                    if len(cfgInclude) > 0:
+                        for inc in includedList:
+                            if entry.title.find(inc) >= 0:
+                                isIncluded = True
+                                logger.info(f"Included '{inc}' has been found in Title: '{entry.title}'")
+                            if entry.description.find(inc) >=0:
+                                logger.info(f"Included '{inc}' has been found in Description: '{entry.description}'")
+                                isIncluded = True
+                    else:
+                        isIncluded = True
+                    if len(cfgExclude) > 0:
+                        for exc in includedList:
+                            if entry.title.find(exc) >= 0:
+                                isExcluded = True
+                                logger.info(f"Excluded '{exc}' has been found in Title: '{entry.title}'")
+                            if entry.description.find(exc) >=0:
+                                logger.info(f"Excluded '{exc}' has been found in Description: '{entry.description}'")
+                                isExcluded = True
+                    else:
+                        isExcluded = False
+                    if isIncluded and not isExcluded:
+                        topics.append(entry.link)
+                else:
+                    logger.info("RSS Entry is too old.")
+        for topic in topics:
+            #logger.debug(topic)
+            for c in completed:
+                if c == topic:
+                    visited=True
+                    completedsave.append(c)
+                    logger.info(f"Topic has already been visited: {c}")
+            if not visited:
+                topicstovisit.append(topic)
+                completedsave.append(topic)
+                logger.info(f"Topic added to list: {topic}")
+            visited=False
 
     #Open Topics and download the torrent file
     for topic in topicstovisit:
@@ -161,12 +225,11 @@ def getNewTorrents():
         logger.info(f'Trying: {topic}')
         try:
             dl = browser.find_element(By.XPATH,'//*[@title="Download torrent"]').click()
-            #logger.debug(dl)
             logger.info(f'Torrent extracted from: {topic}')
         except NoSuchElementException:
             logger.info(f"No new Download found in: {str(topic)}")
 
-    #Write the completed file
+    #Write completed file
     with open(fileCompleted, "w") as foCompleted:
         for i in reversed(completedsave):
             foCompleted.writelines(i + "\n")
